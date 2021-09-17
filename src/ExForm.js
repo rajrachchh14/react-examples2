ae5d06d750d23357b98cdb43e8f9f23b8e23a809
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-17 08:23:05 +0530

    1 file modified

--- src/ExForm.js
+++ src/ExForm.js
@@ -1,14 +1,29 @@
 import React, { useState } from 'react';
 
 export default function App() {
-  const [name, SetName] = useState();
+  let [OldName, SetOldName] = useState();
+  let [NewName, SetNewName] = useState();
+
+  const NameChange = (e) => {
+    SetOldName(e.target.value);
+  };
+
+  const Submit = () => {
+    SetNewName(OldName);
+  };
 
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <h1>hi {name} </h1>
+            <h1>hi {NewName} </h1>
+
+            <input type="text" value={OldName} onChange={NameChange} />
+
+            <button className="btn btn-success" onClick={Submit}>
+              Ok
+            </button>
           </div>
         </div>
       </div>
