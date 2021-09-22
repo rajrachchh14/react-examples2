6585c29ce4fb4f7c161562289f289db4c7fd0e59
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-22 18:45:05 +0530

    1 file modified, 1 added

--- src/According.js
+++ src/According.js
@@ -1,43 +1,21 @@
 import React, { useState } from 'react';
 
 import AccordingQuestion from './AccordingQuestion';
+import AccordingComponent from './AccordingComponent';
 
 export default function According() {
   let [data, setData] = useState(AccordingQuestion);
   let [show, setShow] = useState(false);
 
-  const Open = () => {
-    if (show === false) {
-      setShow(true);
-    } else {
-      setShow(false);
-    }
-  };
-
   return (
     <>
       <div className="container">
-        {data.map((items, index) => (
-          <div className="container">
-            <div className="accordion">
-              <div className="card">
-                <div className="card-header">
-                  <h2 className="mb-0">
-                    <button type="button" className="btn" onClick={Open}>
-                      <i>{!show ? '+' : '-'}</i> {items.title}
-                    </button>
-                  </h2>
-                </div>
-
-                <div className={!show ? 'collapse' : ''}>
-                  <div className="card-body">
-                    <p>{items.info}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
+        <h3>According Example</h3>
+        <section className="info">
+          {data.map((question) => (
+            <AccordingComponent key={question.id} {...question} />
+          ))}
+        </section>
       </div>
     </>
   );
---
+++ src/AccordingComponent.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function AccordingComponent() {
+  let [show, setShow] = useState(false);
+
+  return (
+    <>
+      <div className="container"></div>
+    </>
+  );
+}
