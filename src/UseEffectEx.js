2a425660ff2c8f6c302f602946edba6445b06cc3
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-25 22:47:13 +0530

    1 file modified

--- src/UseEffectEx.js
+++ src/UseEffectEx.js
@@ -3,11 +3,12 @@ import React, { useEffect, useState } from 'react';
 export default function UseEffectEx() {
   const [count, setCount] = useState(0);
   useEffect(() => {
-    console.log('click');
-  }, []);
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <>
+      see out put in title bar also
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </>
