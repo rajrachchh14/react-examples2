fa1a83502b87b2ae9e0fecacc96744d8d1e9fe37
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-26 15:29:56 +0530

    2 files modified

--- src/AxiosEx.js
+++ src/AxiosEx.js
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 export default function AxiosEx() {
   const [num, SetNum] = useState();
+  const [name, SetName] = useState();
+  const [description, SetDescription] = useState();
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get(
+        `https://apitesting78.000webhostapp.com/api/Item/${num}`
+      );
+      console.log(response);
+      SetName(response.data.title);
+      SetDescription(response.data.description);
+      // return await res.json();
+    }
+    getData();
+  });
 
   return (
     <>
       <div className="container">
-        <h1>You Select {num} </h1>
+        <h1>no : {num} </h1>
+        <h1>Name : {name} </h1>
+        <h1>description : {description} </h1>
         <div className="form-group">
           <label>Select </label>
           <select
